f288f5edb34e5e7ac7eea0ea1cfecf24347d97e5
hanh <hanhnv219@gmail.com>
2025-05-13 15:09:13 +0700

    change ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.sathachlaixe"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
