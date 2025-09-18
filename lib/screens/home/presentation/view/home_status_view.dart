564be481ccd1d8a4f4faaaed6374f974c2f29cfe
BaoTinMinhKhang <baotinminhkhang@gmail.com>
2025-09-18 16:19:32 +0700

    Fix build errors

--- lib/screens/home/presentation/view/home_status_view.dart
+++ lib/screens/home/presentation/view/home_status_view.dart
@@ -27,8 +27,6 @@ class HomeStatusView extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(width: 150, height: 150, child: getRadialGauge()),
-                  Container(width: 150, height: 150, child: getRadialGauge()),
                 ],
               ),
             ),
